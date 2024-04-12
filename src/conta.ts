9b175e11757ab8fb812a9d3698e5b020aef26301
Danilo Araújo <daniloaraujo1990@hotmail.com>
2024-04-12 00:41:26 -0300

    implementação dos métodos sacar e depositar

--- src/conta.ts
+++ src/conta.ts
@@ -7,13 +7,34 @@ abstract class Conta {
    * depositar
    */
   public depositar(valor: number) {
-    
+    let data: Date = new Date();
+    this.creditos.push(new Credito(valor, data));
   }
 
   /**
    * sacar
    */
   public sacar(valor: number) {
+
+    if(!this.temSaldoSuficiente(valor)) {
+      console.log('Saldo insuficiente para esta operação');      
+      return;
+    }
+
+    let data: Date = new Date();
+    this.debitos.push(new Debito(valor, data));
     
+  }
+
+  private calcularTotalCredito(): number {
+    return this.creditos.reduce((total, credito) => total + credito.valor, 0);
+  }
+
+  private calcularTotalDebito(): number {
+    return this.debitos.reduce((total, debito) => total + debito.valor, 0)
+  }
+
+  private temSaldoSuficiente(valorDebito: number):boolean {
+    return this.calcularTotalCredito() > (this.calcularTotalDebito() + valorDebito);
   }
 }
