c88b1d3e685baa0311beaa56d1e45d87e7568a6e
Danilo Araújo <daniloaraujo1990@hotmail.com>
2024-04-18 11:54:45 -0300

    Criação da classe Cargo

---
+++ src/cargo.ts
@@ -0,0 +1,29 @@
+import {Funcionario} from "./funcionario";
+
+export class Cargo {
+    private _nome:string;
+    private _funcionario:Funcionario[];
+
+    public constructor(nome:string, funcionario:Funcionario) {
+        if(!funcionario){
+            console.log("É necessário que exista pelo menos um funcionário, para ocupar um cargo!")
+            return;
+        }
+        this.adicionarFuncionario(funcionario)
+        this.nome = nome;
+    }
+    public get nome(): string {
+        return this._nome;
+    }
+
+    public set nome(value: string) {
+        this._nome = value;
+    }
+    public get funcionario(): Funcionario[] {
+        return this._funcionario;
+    }
+
+    public adicionarFuncionario(funcionario:Funcionario) {
+        this._funcionario.push(funcionario);
+    }
+}
